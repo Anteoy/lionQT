2f5dc4068989330509f3fa00483ff84eb20f7361
Anteoy <anteoy@gmail.com>
2018-04-24 00:29:27 +0800

    deal login

--- logindialog.cpp
+++ logindialog.cpp
@@ -21,31 +21,45 @@ LoginDialog::~LoginDialog()
 
 void LoginDialog::on_loginBtn_clicked()
 {
+    if (ui->usrLineEdit->text().trimmed() == "") {
+        QMessageBox::warning(this, tr("Waring"),
+                              tr("请输入用户名!"),
+                              QMessageBox::Yes);
+
+        return;
+    }
+    if (ui->pwdLineEdit->text() == ""){
+        QMessageBox::warning(this, tr("Waring"),
+                              tr("请输入密码!"),
+                              QMessageBox::Yes);
+
+        return;
+    }
     QNetworkRequest network_request;
     QUrl url("http://127.0.0.1:8080/loginR");
-    QByteArray append("user_name=anteoy&pass_word=123");
+    QJsonObject json;
+    json.insert("user_name", ui->usrLineEdit->text().trimmed());
+    json.insert("pass_word", ui->pwdLineEdit->text());
+    QJsonDocument document;
+    document.setObject(json);
+    QByteArray byteArray = document.toJson(QJsonDocument::Compact);
+    QByteArray append("{\"user_name\"=");
+    append.append(ui->usrLineEdit->text().trimmed());
+    append.append("&\"pass_word\"=}");
+    append.append(ui->pwdLineEdit->text());
+
     network_request.setHeader(QNetworkRequest::ContentTypeHeader,"application/json");
     network_request.setUrl(url);
-    QByteArray jsonString = "{\"user_name\": \"anteoy\", \"pass_word\": \"123\"}";
-    QByteArray postDataSize = QByteArray::number(jsonString.size());
+    QByteArray postDataSize = QByteArray::number(byteArray.size());
     network_request.setRawHeader("Content-Length", postDataSize);
-    manager->post(network_request,jsonString);
+    QNetworkReply *reply = manager->post(network_request,byteArray);
+    cout << "getted reply" << endl;
+    QTextCodec *codec = QTextCodec::codecForName("utf8");
+//    QString all = codec->toUnicode(reply->readAll());
+//    qDebug() << all;
+//    reply->deleteLater();
 //    manager->get(QNetworkRequest(QUrl("http://www.qter.org")));
-    // 判断用户名和密码是否正确，
-    // 如果错误则弹出警告对话框
-    if(ui->usrLineEdit->text().trimmed() == tr("zdz") &&
-           ui->pwdLineEdit->text() == tr("123456"))
-    {
-       accept();
-    } else {
-       QMessageBox::warning(this, tr("Waring"),
-                             tr("user name or password error!"),
-                             QMessageBox::Yes);
-       // 清空内容并定位光标
-       ui->usrLineEdit->clear();
-       ui->pwdLineEdit->clear();
-       ui->usrLineEdit->setFocus();
-    }
+
 }
 
 void LoginDialog::replyFinished(QNetworkReply *reply)
@@ -55,4 +69,50 @@ void LoginDialog::replyFinished(QNetworkReply *reply)
     QString all = codec->toUnicode(reply->readAll());
     qDebug() << all;
     reply->deleteLater();
+    QJsonParseError jsonError;
+    QByteArray json = all.toUtf8();
+    QJsonDocument doucment = QJsonDocument::fromJson(json, &jsonError);  // 转化为 JSON 文档
+    if (!doucment.isNull() && (jsonError.error == QJsonParseError::NoError)) {  // 解析未发生错误
+        qDebug() << "pase json ok" <<endl;
+        if (doucment.isObject()) { // JSON 文档为对象
+            QJsonObject object = doucment.object();  // 转化为对象
+            if (object.contains("code")) {  // 包含指定的 key
+                QJsonValue value = object.value("code");  // 获取指定 key 对应的 value
+                if (value.isString()) {  // 判断 value 是否为字符串
+                    QString code = value.toString();  // 将 value 转化为字符串
+                    qDebug() << "code : " << code;
+                    if (code != "200"){
+                        QMessageBox::warning(this, tr("Waring"),
+                                              tr("用户名或密码不正确!"),
+                                              QMessageBox::Yes);
+                        // 清空内容并定位光标
+                        ui->usrLineEdit->clear();
+                        ui->pwdLineEdit->clear();
+                        ui->usrLineEdit->setFocus();
+                        return;
+                    }else{
+                        QJsonValue token_ = object.value("token");
+                        if (token_.isString()){
+                            QString token = token_.toString();
+                            qDebug() << "token : " << token;
+                            accept();
+                        }else{
+                            QMessageBox::warning(this, tr("Waring"),
+                                                  tr("user name or password error!"),
+                                                  QMessageBox::Yes);
+                            // 清空内容并定位光标
+                            ui->usrLineEdit->clear();
+                            ui->pwdLineEdit->clear();
+                            ui->usrLineEdit->setFocus();
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }else {
+        qDebug() << "pase json err" <<endl;
+        qDebug() << jsonError.errorString() <<endl;
+    }
 }
